463eb2143f3cdd4fa4d7e9d9a6e2d4b7e13f675c
Shantnu <ilyshannu@gmail.com>
2026-07-28 14:22:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse(request=request,name="index.html")
+
+@app.post("/chat")
+async def chat(request: Request):
+    data = await request.json()
+    message = data["message"].lower()
+
+    if "hello" in message:
+        reply = "Hello! How are you?"
+    elif "your name" in message:
+        reply = "I am your voice chatbot."
+    elif "bye" in message:
+        reply = "Goodbye! Have a nice day."
+    else:
+        reply = "You said: " + message
+
+    return JSONResponse({"reply": reply})
